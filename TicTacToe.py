2501f6a6d5dee4ccbf7aca0ba2b48f908c5c146d
Otelo2 <josean2009@live.com.mx>
2021-04-16 13:03:45 -0500

    Check for win

--- TicTacToe.py
+++ TicTacToe.py
@@ -12,6 +12,35 @@ def printBoard(board):
         if (counter == 3 or counter == 6 or counter == 9):
             print("\n")
 
+#currentSymbol is the one that the current player is placing down
+def checkWin(board, currentSymbol):
+    #Horizontal top
+    if ((board[0] == currentSymbol) and (board[1] == currentSymbol) and (board[2] == currentSymbol)):
+        print("Someone won!")
+        return True
+    #Vertical left
+    elif ((board[0] == currentSymbol) and (board[3] == currentSymbol) and (board[6] == currentSymbol)):
+        print("Someone won!")
+        return True
+    #Horizontal bottom
+    elif ((board[6] == currentSymbol) and (board[7] == currentSymbol) and (board[8] == currentSymbol)):
+        print("Someone won!")
+        return True
+    #Vertical right
+    elif ((board[2] == currentSymbol) and (board[5] == currentSymbol) and (board[8] == currentSymbol)):
+        print("Someone won!")
+        return True
+    #Diagonal bottom left to top right
+    elif ((board[6] == currentSymbol) and (board[4] == currentSymbol) and (board[2] == currentSymbol)):
+        print("Someone won!")
+        return True
+    #Diagonal top left to bottom right
+    elif ((board[0] == currentSymbol) and (board[4] == currentSymbol) and (board[8] == currentSymbol)):
+        print("Someone won!")
+        return True
+    else:
+        return False
+
 def main():
     board = ["_", "_", "_",
             "_", "_", "_",
@@ -22,16 +51,26 @@ def main():
     while (not finished):
         if (turn == "Player"):
             #print("What is your move, player?")
+            currentSymbol = "X"
             move = int(input("What is your move, player? "))
-            board[move] = "X"
+            board[move] = currentSymbol
             printBoard(board)
             turn = "Computer"
+            finished = checkWin(board, currentSymbol)
+            if finished:
+                continue
         if (turn == "Computer"):
             #print("What is your move, player?")
+            currentSymbol = "O"
             move = int(input("What is your move, computer? "))
-            board[move] = "O"
+            board[move] = currentSymbol
             printBoard(board)
             turn = "Player"
+            finished = checkWin(board, currentSymbol)
+            if finished:
+                continue
+    
+    print("Thanks for playing")
 
 if __name__ == "__main__":
     main()
